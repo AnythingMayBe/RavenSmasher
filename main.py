f6ee8ac9ed6b468c1006e0d5b5a1463cd71d2675
AnythingMayBe <anythingmaybe@protonmail.com>
2022-01-23 15:24:10 +0100

    Added findStrings function

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import re
+from cleanStrings import cleanstrings
 class RavenSmasher:
     def __init__(self):
         super().__init__()
+    
+    def findStrings(self, path):
+        r = []
+        for string in re.findall("[a-zA-Z0-9]+",str(open("/Users/nojo/Downloads/Raven.jar","rb").read())):
+            if string not in cleanstrings:
+                r.append(string)
+        return r
 
 if __name__ == "__main__":
     smasher = RavenSmasher()
