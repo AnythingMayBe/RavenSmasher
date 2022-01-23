3a2c2f0770e5162e07b496ac0d4cb31e7ccda2c0
AnythingMayBe <anythingmaybe@protonmail.com>
2022-01-23 16:46:21 +0100

    Added stringsMod function

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import re
 from cleanStrings import cleanstrings
+from traces import strings
+import time
+import sys
+
 class RavenSmasher:
     def __init__(self):
         super().__init__()
@@ -18,6 +22,19 @@ class RavenSmasher:
         Function for finding strings of a specific path, without checking if they're in cleanStrings
         """
         return re.findall("[a-zA-Z0-9]+",str(open(path,"rb").read()))
+
+    def stringsMod(self, path):
+        """
+        Function for scanning mods to check if it's detected or not
+        """
+        r = {}
+        r["detected"] = 0
+        r["tested"] = 0
+        for string in re.findall("[a-zA-Z0-9]+",str(open(path,"rb").read())):
+            t = time.time()
+            if string in strings.strings: r["detected"] += 1
+            if string not in cleanstrings: r["tested"] += 1
+        return r
 
 if __name__ == "__main__":
     smasher = RavenSmasher()
