0cf12a34ebd77894b82b213b86d492d7465ed125
AnythingMayBe <anythingmaybe@protonmail.com>
2022-01-23 15:31:13 +0100

    Added findUncheckedStrings function

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ class RavenSmasher:
         for string in re.findall("[a-zA-Z0-9]+",str(open(path,"rb").read())):
             if string not in cleanstrings: r.append(string)
         return r
+    
+    def findUncheckedStrings(self, path):
+        """
+        Function for finding strings of a specific path, without checking if they're in cleanStrings
+        """
+        return re.findall("[a-zA-Z0-9]+",str(open(path,"rb").read()))
 
 if __name__ == "__main__":
     smasher = RavenSmasher()
